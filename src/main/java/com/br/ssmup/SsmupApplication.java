574181620fb28632e5c7160ffddc650b5d7b2a21
DanielDev2003 <everaldodaniel@hotmail.com>
2025-10-29 16:03:23 -0300

    initial commit

---
+++ src/main/java/com/br/ssmup/SsmupApplication.java
@@ -0,0 +1,13 @@
+package com.br.ssmup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ssmup/SsmupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ssmup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
