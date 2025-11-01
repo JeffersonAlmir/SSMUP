6c654dd21cf50315d1a221b595eef7c7485ee084
DanielDev2003 <everaldodaniel@hotmail.com>
2025-11-01 09:50:38 -0300

    feat(exeception): adicionar campo 'path' nas respostas das execeções

--- src/main/java/com/br/ssmup/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/br/ssmup/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.br.ssmup.exceptions;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,22 +20,25 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException e) {
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request) {
         Map<String, Object> response = new LinkedHashMap<>();
         response.put("timestamp", LocalDateTime.now());
         response.put("status", HttpStatus.NOT_FOUND.value());
         response.put("error", "Recurso nâo encontrado");
         response.put("message", e.getMessage());
+        response.put("path", request.getRequestURI());
+
 
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
         Map<String, Object> response = new LinkedHashMap<>();
         response.put("timestamp", LocalDateTime.now());
         response.put("status", HttpStatus.BAD_REQUEST.value());
         response.put("error", "Erro de validacao");
+        response.put("path", request.getRequestURI());
 
         List<Map<String, String>> errors = e.getBindingResult()
                 .getFieldErrors()
@@ -51,23 +55,26 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException e, HttpServletRequest request) {
         Map<String, Object> response = new LinkedHashMap<>();
         response.put("timestamp", LocalDateTime.now());
         response.put("status", HttpStatus.CONFLICT.value());
         response.put("error", "Violação de integridade de dados");
         response.put("message", e.getMessage());
+        response.put("path", request.getRequestURI());
 
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException e, HttpServletRequest  request) {
         Map<String, Object> response = new LinkedHashMap<>();
         response.put("timestamp", LocalDateTime.now());
         response.put("status", HttpStatus.BAD_REQUEST.value());
         response.put("error", "Requisição malformada");
         response.put("message", e.getMessage());
+        response.put("path", request.getRequestURI());
+
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
