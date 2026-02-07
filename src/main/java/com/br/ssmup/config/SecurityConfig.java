0711149a0dc3ec984774d7a86ec23cccaaa8c088
dnielbr <everaldodaniel@hotmail.com>
2026-02-06 21:57:28 -0300

    feat: Adiciona dependencia do OpenApi(Swagger) e uma classe de configuração OpenApiConfig. Vai ser utilizado para fazer a documentação completa da api.

--- src/main/java/com/br/ssmup/config/SecurityConfig.java
+++ src/main/java/com/br/ssmup/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers(HttpMethod.POST, "/v1/api/auth/google").permitAll();
                     req.requestMatchers("/actuator/health").permitAll();
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll();
                     req.requestMatchers("v1/api/usuarios/**").hasRole("ADMIN");
                     req.anyRequest().authenticated();
                 })
