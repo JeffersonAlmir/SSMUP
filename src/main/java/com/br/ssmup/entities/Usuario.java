446ac294640d1759c713073407458f76b61e6005
dnielbr <everaldodaniel@hotmail.com>
2026-03-21 10:04:44 -0300

    refactor: Adicionar anotações do lombook a classe de Usuario com o intuito de diminuir boilerplaite

--- src/main/java/com/br/ssmup/entities/Usuario.java
+++ src/main/java/com/br/ssmup/entities/Usuario.java
@@ -2,16 +2,20 @@ package com.br.ssmup.entities;
 
 import com.br.ssmup.enums.Role;
 import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name = "tb_usuario")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Usuario implements UserDetails {
 
     @Id
@@ -35,18 +39,12 @@ public class Usuario implements UserDetails {
     private Role role;
 
     @Column(nullable = false)
-    private boolean ativo = true;
+    private boolean ativo = false;
 
-    public Usuario() {};
+    @Column(unique = true)
+    private String tokenAtivacao;
 
-    public Usuario(Long id, String nome, String email, String cargo, String matricula, Role role) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.cargo = cargo;
-        this.matricula = matricula;
-        this.role = role;
-    }
+    private LocalDateTime dataExpiracaoToken;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -70,85 +68,6 @@ public class Usuario implements UserDetails {
     @Override
     public boolean isCredentialsNonExpired() { return true; }
     @Override
-    public boolean isEnabled() { return true; }
+    public boolean isEnabled() { return this.ativo; }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return Objects.equals(id, usuario.id) && Objects.equals(nome, usuario.nome) && Objects.equals(email, usuario.email) && Objects.equals(cargo, usuario.cargo) && Objects.equals(matricula, usuario.matricula) && role == usuario.role;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, email, cargo, matricula, role);
-    }
-
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                ", cargo='" + cargo + '\'' +
-                ", matricula='" + matricula + '\'' +
-                ", role=" + role +
-                '}';
-    }
 }
